3a211830f7d65046f2ef5f8f958367f1915f4033
John <johnnkonge2020@gmail.com>
2023-10-25 12:27:30 +0300

    Done deliverables Added some style

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ function NavBar() {
   return (
     <nav>
       <a href="#home">I'm a link!</a>
+      <a href="#about">About Me</a>
     </nav>
   );
 }
@@ -18,13 +19,22 @@ function Home() {
 
 {/* write an <About> component here */}
 
+function About(){
+  return (
+    <div id="about" >
+      <h1>About</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home/>
       {/* add your <About> component here */}
+      <About/>
     </div>
   );
 }
